d67f120196a0c4f6bb49af9c64f5721257b3893e
tangyanghai <tangyanghai@wshifu.com>
2020-04-24 16:36:32 +0800

    极光设置

---
+++ app/src/main/java/com/example/administrator/foregroundservicetest/PushSettingActivity.java
@@ -0,0 +1,12 @@
+package com.example.administrator.foregroundservicetest;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * <p>@author : tangyanghai</p>
+ * <p>@time : 2020/4/24</p>
+ * <p>@for : </p>
+ * <p></p>
+ */
+public class PushSettingActivity extends AppCompatActivity{
+}
